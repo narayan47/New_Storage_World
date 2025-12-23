dfb829af2b3c75a138a79c3526114b8bca0ec880
narayan47 <narayancharan865@gmail.com>
2025-12-23 12:14:16 +0530

    make some change in cloudinary config

--- backend/config/cloudinary.js
+++ backend/config/cloudinary.js
@@ -1,9 +1,10 @@
-
+import dotenv from "dotenv"
+dotenv.config({path:"./.env"})
 import { v2 as cloudinary } from "cloudinary";
 cloudinary.config({
-    cloud_name:'dgymta3cu',
-    api_key:'148144824984144',
-    api_secret:'4zn9jdudhXN5bAPBywXDU7cQsR4'
+    cloud_name:process.env.CLOUDINARY_CLOUD_NAME,
+    api_key:process.env.CLOUDINARY_API_KEY,
+    api_secret:process.env.CLOUDINARY_API_SECRET
 
 })
 
